af764372a46681b81cbbe8e1fa1c6258f15353e4
Matheus <matheusgaldinoinfo@gmail.com>
2025-06-18 23:27:46 -0300

    create buffer to send bytes in post requests

---
+++ http-post/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	c := http.Client{}
+
+	jsonVar := bytes.NewBuffer([]byte(`{"name":"John", "age":30}`))
+
+	resp, err := c.Post("https://www.google.com", "application/json", jsonVar)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	io.CopyBuffer(os.Stdout, resp.Body, nil)
+}
